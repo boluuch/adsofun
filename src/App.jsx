2db921d239b1777dff7a930fb1c4f259dbd8de3f
Ngozi Uchenna <uchennaenk@gmail.com>
2023-08-26 13:50:40 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Careers from "./pages/careers";
+import Products from "./pages/products";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Contact from "./pages/contact";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        {/* <Header /> */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/careers" element={<Careers />} />
+          <Route path="/contact" element={<Contact/>} />
+          <Route path="/products" element={<Products />} />
+        </Routes>
+      </BrowserRouter>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
